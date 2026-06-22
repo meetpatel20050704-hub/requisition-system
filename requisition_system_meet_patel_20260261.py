f6776b408a238da41f5c309a0db4b6567f99a352
Meet Patel <meetnptl.aus061@gmail.com>
2026-06-23 11:29:59 +1200

    Added Assessment 2 requisition system code

--- requisition_system_meet_patel_20260261.py
+++ requisition_system_meet_patel_20260261.py
@@ -1,14 +1,13 @@
 class Requisition:
-    counter = 10000
-    all_requisitions = []
-
-    def __init__(self, date, staff_id, staff_name):
-        Requisition.counter += 1
-        self.date = date
+    def __init__(self, staff_id, staff_name):
         self.staff_id = staff_id
         self.staff_name = staff_name
-        self.requisition_id = Requisition.counter
-        self.items = []
-        self.total = 0
-        self.status = "Pending"
-        self.approval_reference = "Not available"
+
+    def display_staff(self):
+        print("Staff ID:", self.staff_id)
+        print("Staff Name:", self.staff_name)
+
+
+# Test Object
+req = Requisition("FN19", "John Paul")
+req.display_staff()
